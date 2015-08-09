fd5f8d03609e29021923e811e9975f70b78cc377
jianmink <jianmink.taihe@gmail.com>
2015-08-10 07:11:56 +0800

    ...

--- src/e164cc.py
+++ src/e164cc.py
@@ -17,19 +17,78 @@ def load():
 
 
 def e164cc(vlrnum):
-    begin = time.time()
     cc = ""
     for each in e164ccList:
-        if vlrnum.startswith(each):
+        if vlrnum.startswith(each) and len(each) > len(cc):
             cc = each 
-            break
+            
+    return cc
+
+
+
+
+
+dictE164Cc={}
+
+def addE164Cc(cc):
+    global dictE164Cc
     
+    dict_ = dictE164Cc
+    for c in cc[:-1]:
+        if c not in dict_.keys():
+            dict_[c]={}
+            
+        dict_=dict_[c]
+
+    if cc[-1] in dict_.keys():
+        dict_[cc[-1]]
+    else:
+        dict_[cc[-1]] = cc
+    
+
+def loadplus(filename):
+    with open(filename) as f:
+        for record in f:
+            fields =record.split(',')
+            cc = fields[0].strip()
+            print cc
+            addE164Cc(cc)
+            
+
+
+def e164ccplus(vlrnum):
+    
+    pass
+
+
+def genVlrnumList(n):
+    import random
+    vlrnums=[]
+    for _ in range(n):
+        vlrnum = ""
+        len_ = 3+int(random.random()*10)%10
+        for _ in range(len_):
+            vlrnum+="%d" %(int(random.random()*10)%10)
+        
+        vlrnums.append(vlrnum+"\n")
+        
+    with open("./vlrnumlist%d"%(n), 'w+') as f:
+        f.writelines(vlrnums)
+        
+
+
+if __name__ == "__main__":
+    begin = time.time()
+    load()
+    with open("vlrnumList10000",'r') as f:
+        for vlrnum in f:
+            e164cc(vlrnum)
+    
+    e164cc("91500") 
     end = time.time()
     delta = (end - begin)
-    print begin
-    print end
-    print "delta: %.19f us" %delta 
-    return cc
+    print "delta: %.06f ms" %(delta*1000) 
+
 
 class TestE164cc(unittest.TestCase):
     
